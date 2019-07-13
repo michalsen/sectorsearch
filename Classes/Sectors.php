1255a08ad9705328e426eb30bade263b6b3ac2c6
Eric Michalsen <eric.michalsen@gmail.com>
2019-07-13 13:25:38 -0400

    sector array build

--- Classes/Sectors.php
+++ Classes/Sectors.php
@@ -18,40 +18,40 @@ class Sectors {
 
      $sectors = [
                   ['name' => 'Sa', 'compute' => ['-1', '-1', '-1']],
-                  ['name' => 'Sb', 'compute' => ['', '-1', '-1']],
+                  ['name' => 'Sb', 'compute' => ['+0', '-1', '-1']],
                   ['name' => 'Sc', 'compute' => ['+1', '-1', '-1']],
-                  ['name' => 'Sd', 'compute' => ['-1', '', '-1']],
-                  ['name' => 'Se', 'compute' => ['', '', '-1']],
-                  ['name' => 'Sf', 'compute' => ['+1', '', '-1']],
+                  ['name' => 'Sd', 'compute' => ['-1', '+0', '-1']],
+                  ['name' => 'Se', 'compute' => ['+0', '+0', '-1']],
+                  ['name' => 'Sf', 'compute' => ['+1', '+0', '-1']],
                   ['name' => 'Sg', 'compute' => ['-1', '+1', '-1']],
-                  ['name' => 'Sh', 'compute' => ['', '+1', '-1']],
+                  ['name' => 'Sh', 'compute' => ['+0', '+1', '-1']],
                   ['name' => 'Si', 'compute' => ['+1', '+1', '-1']],
                   ['name' => 'Ta', 'compute' => ['-1', '-1', '-1']],
-                  ['name' => 'Tb', 'compute' => ['', '-1', '-1']],
+                  ['name' => 'Tb', 'compute' => ['+0', '-1', '-1']],
                   ['name' => 'Tc', 'compute' => ['+1', '-1', '-1']],
-                  ['name' => 'Td', 'compute' => ['-1', '', '-1']],
-                  ['name' => 'Te', 'compute' => ['', '', '-1']],
-                  ['name' => 'Tf', 'compute' => ['+1', '', '-1']],
+                  ['name' => 'Td', 'compute' => ['-1', '+0', '-1']],
+                  ['name' => 'Te', 'compute' => ['+0', '+0', '+0']],
+                  ['name' => 'Tf', 'compute' => ['+1', '+0', '-1']],
                   ['name' => 'Tg', 'compute' => ['-1', '+1', '-1']],
-                  ['name' => 'Th', 'compute' => ['', '+1', '-1']],
+                  ['name' => 'Th', 'compute' => ['+0', '+1', '-1']],
                   ['name' => 'Ti', 'compute' => ['+1', '+1', '-1']],
                   ['name' => 'Za', 'compute' => ['-1', '-1', '-1']],
-                  ['name' => 'Zb', 'compute' => ['', '-1', '-1']],
+                  ['name' => 'Zb', 'compute' => ['+0', '-1', '-1']],
                   ['name' => 'Zc', 'compute' => ['+1', '-1', '-1']],
-                  ['name' => 'Zd', 'compute' => ['-1', '', '-1']],
-                  ['name' => 'Ze', 'compute' => ['', '', '-1']],
-                  ['name' => 'Zf', 'compute' => ['+1', '', '-1']],
+                  ['name' => 'Zd', 'compute' => ['-1', '+0', '-1']],
+                  ['name' => 'Ze', 'compute' => ['+0', '+0', '-1']],
+                  ['name' => 'Zf', 'compute' => ['+1', '+0', '-1']],
                   ['name' => 'Zg', 'compute' => ['-1', '+1', '-1']],
-                  ['name' => 'Zh', 'compute' => ['', '+1', '-1']],
+                  ['name' => 'Zh', 'compute' => ['+0', '+1', '-1']],
                   ['name' => 'Zi', 'compute' => ['+1', '+1', '-1']],
                 ];
 
 
       foreach ($sectors as $key => $value) {
-        print $value['compute'][0] . '<br>';
-        $compute = compute($breakdown[0], $value['compute'][0]);
-        print  $compute . '<br>';
-        // $this->{$value['name']} = $sector->readSector(($breakdown[0] . $compute[0]) . '-' . ($breakdown[1] . $compute[1]) . '-' . ($breakdown[2] . $compute[2]));
+        $compute0  = compute($breakdown[0], $value['compute'][0]);
+        $compute1  = compute($breakdown[1], $value['compute'][1]);
+        $compute2  = compute($breakdown[2], $value['compute'][2]);
+        $this->{$value['name']} = $sector->readSector($compute0 . '-' . $compute1 . '-' .  $compute2);
       }
 
     }
