d5da7d2a9d5367df1811e2ef7f1cd3d4ee3ab7ba
Eric Michalsen <eric.michalsen@gmail.com>
2019-07-15 13:13:12 -0400

    sector display

--- assets/ajax/ajax.php
+++ assets/ajax/ajax.php
@@ -5,6 +5,8 @@ require '../../Classes/Resource.php';
 require '../../includes/rb.php';
 R::setup('mysql:host=database;dbname=lamp','lamp', 'lamp');
 
+
+
 $return = '';
 if (count($_REQUEST) > 0) {
   foreach ($_REQUEST as $type => $item) {
---
+++ assets/ajax/sector.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+require_once '../../vendor/autoload.php';
+require '../../Classes/Resource.php';
+require '../../includes/rb.php';
+R::setup('mysql:host=database;dbname=lamp','lamp', 'lamp');
+
+
+
+// $return = '';
+// if (count($_REQUEST) > 0) {
+//   foreach ($_REQUEST as $type => $item) {
+//     $return .= $type . ': ' . $item . '<br>';
+
+//       $purchase = new Resource();
+//       $purchaseID = $purchase->resourcePurchase($type, $item);
+
+//     echo 'purchaseID: ' . $purchaseID;
+
+//   }
+// }
+//  else {
+//   $return = 'nope';
+//  }
+
+echo 'test';
