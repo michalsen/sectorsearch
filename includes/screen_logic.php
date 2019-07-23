d799306deb9a29d2596227525da73f560779bdc7
Eric Michalsen <eric.michalsen@gmail.com>
2019-07-23 13:03:59 -0400

    Resources Class playerResources()

--- includes/screen_logic.php
+++ includes/screen_logic.php
@@ -49,14 +49,14 @@ switch ($return['screen']) {
 
     $sectors = new Sectors($playerData['coordinates']);
 
-    $Resource = playerResources($_SESSION['player'], $stats, $playerdata);
+    // $Resource = playerResources($_SESSION['player'], $stats, $playerdata);
 
     $template = $twig->loadTemplate('home.html');
 
     echo $template->render(array(
               'player' => $player,
               // 'playerdata' => $playerData,
-              'Resources' => $Resource['Resources'],
+              // 'Resources' => $Resource['Resources'],
               'Sectors' => $sectors,
             )
           );
@@ -79,47 +79,47 @@ switch ($return['screen']) {
 
 
 
-function playerResources($id, $stats, $playerdata) {
+// function playerResources($id, $stats, $playerdata) {
 
-  $startTime = $stats['start'];
-  $currentTime = date('U');
+//   $startTime = $stats['start'];
+//   $currentTime = date('U');
 
 
-  $purchases = R::find('purchase',' player = ? ',
-         array( $_SESSION['player'] )
-       );
+//   $purchases = R::find('purchase',' player = ? ',
+//          array( $_SESSION['player'] )
+//        );
 
-  $metal = 0;
-  $fuel = 0;
-  $food = 0;
+//   $metal = 0;
+//   $fuel = 0;
+//   $food = 0;
 
-  foreach ($purchases as $key => $value) {
-    if ($value->type == 'purchase') {
-      if ($value->item == 'miner') {
-        $metal = $metal + 500;
-        $fuel = $fuel + 200;
-        $food = $food + 300;
-      }
-      if ($value->item == 'scout') {
-        $metal = $metal + 200;
-        $fuel = $fuel + 800;
-        $food = $food + 200;
-      }
-    }
-  }
+//   foreach ($purchases as $key => $value) {
+//     if ($value->type == 'purchase') {
+//       if ($value->item == 'miner') {
+//         $metal = $metal + 500;
+//         $fuel = $fuel + 200;
+//         $food = $food + 300;
+//       }
+//       if ($value->item == 'scout') {
+//         $metal = $metal + 200;
+//         $fuel = $fuel + 800;
+//         $food = $food + 200;
+//       }
+//     }
+//   }
 
-  $resourceAllotment = ($currentTime - $startTime)/10000000 * $stats['level'];
+//   $resourceAllotment = ($currentTime - $startTime)/10000000 * $stats['level'];
 
-  $upper = $playerdata['moons'] * 2;
-  $Resource = [];
-  $Resource['power'] = round($playerdata['sun'] * 3.2 * $upper + $resourceAllotment);
-  $Resource['material'] = round($playerdata['rocky_planets'] * 3 * $upper + $resourceAllotment);
-  $Resource['food'] = round($playerdata['goldilocks_planets'] * 2.5 * $upper + $resourceAllotment) - $food;
-  $Resource['metal'] = round($playerdata['belts'] * 2.2 * $upper + $resourceAllotment) - $metal;
-  $Resource['fuel'] = round($playerdata['gas_planets'] * 2.1 * $upper + $resourceAllotment) - $fuel;
+//   $upper = $playerdata['moons'] * 2;
+//   $Resource = [];
+//   $Resource['power'] = round($playerdata['sun'] * 3.2 * $upper + $resourceAllotment);
+//   $Resource['material'] = round($playerdata['rocky_planets'] * 3 * $upper + $resourceAllotment);
+//   $Resource['food'] = round($playerdata['goldilocks_planets'] * 2.5 * $upper + $resourceAllotment) - $food;
+//   $Resource['metal'] = round($playerdata['belts'] * 2.2 * $upper + $resourceAllotment) - $metal;
+//   $Resource['fuel'] = round($playerdata['gas_planets'] * 2.1 * $upper + $resourceAllotment) - $fuel;
 
-  $playerdata['Resources'] = $Resource;
+//   $playerdata['Resources'] = $Resource;
 
-  return $playerdata;
+//   return $playerdata;
 
-}
+// }
